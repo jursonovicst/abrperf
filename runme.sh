59ef1edb0940374dbfafbba8f298e8e410882c97
jursonovicst <github.com@jursonovics.com>
2020-08-25 13:42:39 +0200

    logging and ssh background processes...

--- runme.sh
+++ runme.sh
@@ -9,21 +9,25 @@ numslaves=2
 
 for host in ${slavehosts[*]}; do
   # sync config
+  echo "syncing config with slave $host..."
   scp ./abrperf.ini $host:abrperf/
   scp ./urllist.csv $host:abrperf/
 
   # start slaves
-  ssh $host "rm ~/abrperf/slaves.pid"
+  ssh $host "rm -f ~/abrperf/slaves.pid"
   for i in $(seq 1 $numslaves); do
-    ssh $host "cd ~/abrperf/ && source venv/bin/activate && nohup locust -f locustfiles/hlsplayer.py --worker --master-host=$masterhost >/dev/null 2>&1 & echo \$! >> slaves.pid"
+    echo "starting locust worker $i on slave $host..."
+    ssh $host "cd ~/abrperf/ && source venv/bin/activate && nohup locust -f locustfiles/hlsplayer.py --worker --master-host=$masterhost &>/dev/null </dev/null & echo \$! >> slaves.pid"
   done
 done
 
 # start master and wait for exit
+echo "starting master, terminate it with CTRL+C"
 source venv/bin/activate && locust -f locustfiles/hlsplayer.py --master
 
 # termiante slaves
 for host in ${slavehosts[*]}; do
+  echo "terminating still running workers on slave $host..."
   ssh $host "cat ~/abrperf/slaves.pid | xargs kill -9"
 
 done
