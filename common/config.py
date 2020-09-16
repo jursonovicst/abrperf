e43a3d0dcfdca17c60adbd6ff99a2fccadc6ce1d
jursonovicst <github.com@jursonovics.com>
2020-09-16 21:13:26 +0200

    comments and error handling

--- common/config.py
+++ common/config.py
@@ -6,24 +6,36 @@ import numpy as np
 
 
 class Config:
+    """
+    Basic config file
+    """
 
     def __init__(self):
-        # read config
+        # read config, in case of error, the config will contain an empty dataset. Use defaults everywhere!
         self._config = configparser.ConfigParser()
         self._config.read('abrperf.ini')
 
-        # load live URLs and weights
+        # load URLs and weights
         self._urls = []
         self._weights = []
         self._urllist = self._config.get('global', 'urllist', fallback='urllist.csv')
 
         with open(self._urllist, newline='', ) as csvfile:
-            spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            lines = csv.reader(csvfile, delimiter=',', quotechar='"')
 
-            for row in spamreader:
-                self._urls.append(row[0])
+            if lines.line_num == 0:
+                raise SyntaxError(
+                    f"Empty urllist file '{self._urllist}'!")
+
+            for row in lines:
+                if len(row) != 2:
+                    raise SyntaxError(f"Invalid urllist file '{self._urllist}' content: '{', '.join(row)}'!")
+
+                self._urls.append(str(row[0]))
                 if not row[1].strip().isdigit():
-                    raise ValueError(f"In the {self._urllist} csv, I expect positive integers, but got '{row[1]}'")
+                    raise ValueError(
+                        f"Positive integers expected in urllist file '{self._urllist}', but got '{row[1]}'!")
+
                 self._weights.append(int(row[1]))
 
         # load profile selection
@@ -31,7 +43,7 @@ class Config:
         if self._profileselection not in ['max', 'min', 'random']:
             raise SyntaxError(f"Profileselection '{self._profileselection}' is not supported!")
 
-    def __str__(self):
+    def __str__(self) -> str:
         buff = ''
         for section in self._config.sections():
             buff += f"[{section}]\n"
@@ -39,7 +51,7 @@ class Config:
                 buff += f"{item}: {self._config[section][item]}\n"
         return buff
 
-    def getrandomurl(self):
+    def getrandomurl(self) -> str:
         """
         Returns a randomly chosen URL from the urllist.csv according to the weights specified.
         :return: A randomly chosen URL
@@ -47,11 +59,12 @@ class Config:
         """
         return random.choices(self._urls, weights=self._weights, k=1)[0]
 
-    def profilemethod(self):
+    def profilemethod(self) -> callable:
         """
         Returns a function, which selects the appropriate profile (variant playlist) according to the config file. The
         returned function requires two parameters: first argument is a list of objects to select, the second named
         argument (key=) is a function, based on which the selection is done.
+        _rtype: callable
         """
         if self._profileselection == 'max':
             return lambda population, key: max(population, key=key)
@@ -61,6 +74,9 @@ class Config:
 
 
 class LivePlayerConfig(Config):
+    """
+    Extended config file for live playback
+    """
 
     def __init__(self):
         super().__init__()
@@ -72,9 +88,9 @@ class LivePlayerConfig(Config):
             if self._timeshift < 0:
                 raise ValueError(f"Typical seek time must be non negative, got '{self._timeshift}'")
 
-    def gettimeshift(self):
+    def gettimeshift(self) -> int:
         """
-        Returns a random timeshift value for a user.
+        Returns a poisson distributed random value for timeshift.
         :return: Timeshift in seconds
         :rtype: int
         """
