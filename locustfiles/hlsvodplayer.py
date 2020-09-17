e943f09dd64413bb2b3a7f47071ec682617393f3
jursonovicst <github.com@jursonovics.com>
2020-09-17 11:58:32 +0200

    finetune logging add playlist type

--- locustfiles/hlsvodplayer.py
+++ locustfiles/hlsvodplayer.py
@@ -8,6 +8,7 @@ import m3u8
 import time
 from common import Config
 import platform
+import names
 
 
 @events.init.add_listener
@@ -26,7 +27,7 @@ def on_locust_init(environment, **kwargs):
 
     # read configuration (worker level)
     try:
-        environment.config = Config()
+        environment.config = Config(logging)
         logging.info(f"Config loaded.")
     except Exception as e:
         logging.error(e)
@@ -49,14 +50,13 @@ class PlayVoD(TaskSet):
             self.interrupt(reschedule=False)
 
         segment = self.parent._m3u8.segments.pop(0)
-        logging.debug(f"Segment URL: '{segment.uri}'")
-
-        seg_get = self.client.get(segment.absolute_uri, name=f"{segment.uri}")
+        self.user.logger.debug(f"Get segment: '{segment.uri}'")
+        self.client.get(segment.absolute_uri, name=f"{segment.uri}")
 
         # calculate the remaining time till next segment fetch
         ts_stop = time.time()
         if segment.duration - (ts_stop - ts_start) < 0:
-            logging.warning(f"segment over time request")
+            self.user.logger.warning(f"segment over time request")
         self.wait_left = max(0, segment.duration - (ts_stop - ts_start))
 
     def wait_time(self):
@@ -81,8 +81,8 @@ class PlayStream(SequentialTaskSet):
         super().__init__(*args, **kwargs)
         self._config = None
         self._manifest_url = ''
-        self._base_url = ''
         self._m3u8 = None
+        self._type = None
 
     def on_start(self):
         """
@@ -93,12 +93,13 @@ class PlayStream(SequentialTaskSet):
         """
         # load config
         self._config = self.client.environment.config
-        logging.info(f"Starting streaming client.")
 
         # get manifest url
         self._manifest_url = self._config.getrandomurl()
-        logging.debug(f"Playlist URL: '{self._manifest_url}'")
-        self._base_url = os.path.dirname(self._manifest_url)
+        self.user.logger.debug(f"Play stream URL: '{self._manifest_url}'.")
+
+    def on_stop(self):
+        self.user.logger.debug(f"Stream stop.")
 
     @task
     def manifest(self):
@@ -106,49 +107,48 @@ class PlayStream(SequentialTaskSet):
         1st parse the manifest, in case of a master manifest, reparse it.
         """
         # get manifest
+        self.user.logger.debug(f"Get manifest '{self._manifest_url}'")
+        base_url = os.path.dirname(self._manifest_url)
         manifest = self.client.get(self._manifest_url, name=f"{os.path.basename(self._manifest_url)}")
 
         if not isinstance(manifest, FastResponse):
-            logging.error(f"Error accessing playlist: {vars(manifest)}")
+            self.user.logger.error(f"Error accessing playlist: {vars(manifest)}")
             self.interrupt(reschedule=False)
 
         if 'Content-Type' not in manifest.headers:
-            logging.error(f"No Content-Type received, terminating streaming client.")
-            self.interrupt(reschedule=False)
+            self.user.logger.warning(f"No Content-Type received.")
 
         if manifest.headers['Content-Type'] != 'application/vnd.apple.mpegurl':
-            # unknown streaming
-            logging.error(f"Unknown stream with Content-Type '{manifest.headers['Content-Type']}', terminating.")
-            self.interrupt(reschedule=False)
+            self.user.logger.error(f"Unknown stream with Content-Type '{manifest.headers['Content-Type']}'.")
 
         # it must be a m3u8 object, parse
-        self._m3u8 = m3u8.M3U8(content=manifest.text, base_uri=self._base_url)
+        self._m3u8 = m3u8.M3U8(content=manifest.text, base_uri=base_url)
         #        logging.debug(f"EXT-X-VERSION: '{master_m3u8}'") TODO: check why this is not working
 
         if self._m3u8.is_variant:
-            logging.debug(f"Master playlist detected: '{self._manifest_url}'")
+            self.user.logger.debug(f"Master playlist detected: '{self._manifest_url}'")
 
             # Select profile according to the profile selection config
             profilemethod = self._config.profilemethod()
             playlist = profilemethod(self._m3u8.playlists, key=lambda pl: pl.stream_info.bandwidth)
-            logging.info(
+            self.user.logger.debug(
                 f"Profile {playlist.stream_info.program_id} selected (bandwidth: {playlist.stream_info.bandwidth}, resolution: {playlist.stream_info.resolution})")
 
-            self._manifest_url = f"{self._base_url}/{playlist.uri}"
-            logging.debug(f"Selected variant: {self._manifest_url}")
+            self._manifest_url = f"{base_url}/{playlist.uri}"
+            self.user.logger.debug(f"Selected variant: {self._manifest_url}")
 
             # parse variant playlist
             self.manifest()
 
         else:
-            logging.debug(f"Variant playlist detected: '{self._manifest_url}'")
+            self._type = self._m3u8.playlist_type.upper()
+            self.user.logger.debug(f"Variant {self._type} playlist detected: '{self._manifest_url}'")
 
     """
     2nd: get the segments
     """
     tasks = [PlayVoD]
 
-    @task
     def stop(self):
         """
         3rd if streaming ends, terminates
@@ -163,7 +163,29 @@ class PlayStream(SequentialTaskSet):
 
 
 class MyLocust(FastHttpUser):
+    # Play stream
     tasks = [PlayStream]
 
     # we need to specify this, but it will be ignored
     wait_time = constant(0)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._logger = None
+
+    @property
+    def logger(self):
+        return self._logger
+
+    def on_start(self):
+        """
+        User and TaskSet classes can declare an on_start method and/or on_stop method. A User will call it’s on_start
+        method when it starts running, and it’s on_stop method when it stops running. For a TaskSet, the on_start method
+        is called when a simulated user starts executing that TaskSet, and on_stop is called when the simulated user
+        stops executing that TaskSet (when interrupt() is called, or the user is killed).
+        """
+        self._logger = logging.getLogger(names.get_full_name())
+        self.logger.info(f"User spawned.")
+
+    def on_stop(self):
+        self.logger.info(f"User terminated.")
